e2268a9e9921fc89f5b8b2c628d092de27c4461e
Usachev Daniel <lordsmistykus@gmail.com>
2025-11-18 23:24:11 +0300

    new func for main.py and updated Readme

--- Lab_1/main.py
+++ Lab_1/main.py
@@ -1,1 +1,12 @@
 print("Night labs speedrun")
+n = 4
+for i in range(n):
+    # Левые пробелы
+    spaces = ' ' * (n - i - 1)
+    # Звезды
+    stars = '*' * (2 * i + 1)
+    # Правые пробелы
+    right_spaces = ' ' * (n - i - 1)
+    
+    print(spaces + stars + right_spaces)
+print("sverhu piramida))")
